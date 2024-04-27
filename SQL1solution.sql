a769c94d519d389613990dd03fb2eb26a94e89fc
Pooja Madhup <90482825+pooja9050@users.noreply.github.com>
2024-04-27 08:50:58 -0700

    Update SQL1solution.sql

--- SQL1solution.sql
+++ SQL1solution.sql
@@ -1,4 +1,23 @@
-# Write your MySQL query statement below
+# 595. Probelm 1. Big Countries .Write your MySQL query statement below
 Select name, population, area
 From World
 Where area >=3000000 OR population >= 25000000;
+
+#177. Problem 2. Nth Highest Salary
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+RETURN (
+# Write your MySQL query statement below.
+With CTE AS (
+    Select *, DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
+    FROM Employee)
+    SELECT DISTINCT IFNULL (salary, null)  from CTE 
+    WHERE rnk = N
+ );
+END
+
+#196. Problem 3. Delete Duplicate Emails
+# Write your MySQL query statement below
+DELETE p FROM  Person p, Person b
+WHERE p.email = b.email and p.id > b.id;
+
